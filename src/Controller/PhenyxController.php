d41a92afb1c52f69383ebe76d434df7719bd3663
Near EarthObject <55352817+ephenyxshop@users.noreply.github.com>
2023-05-28 12:41:04 +0200

    5.4.5

--- src/Controller/PhenyxController.php
+++ src/Controller/PhenyxController.php
@@ -618,6 +618,9 @@ abstract class PhenyxController {
         $this->addJS([
             'https://code.jquery.com/jquery-3.6.4.min.js',
             'https://code.jquery.com/ui/1.13.2/jquery-ui.min.js',
+
+        ]);      
+        $this->addFooterJS([           
            	'https://cdn.ephenyx.io/paramgrid/pqSelect/pqselect.min.js',
            	'https://cdn.ephenyx.io/paramgrid/pqgrid.min.js',
            	'https://cdn.ephenyx.io/paramgrid/localize/pq-localize-fr.js',
@@ -627,7 +630,7 @@ abstract class PhenyxController {
            	'https://cdn.ephenyx.io/paramgrid/javascript-detect-element-resize/detect-element-resize.js',
            	'https://cdn.ephenyx.io/paramgrid/javascript-detect-element-resize/jquery.resize.js',			
 
-        ]);       
+        ]);      
     }
 
     abstract public function checkAccess();
