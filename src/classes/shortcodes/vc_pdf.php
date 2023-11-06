629cb81cf163a70bb9fbec276bef5d00b5c82b14
Near EarthObject <55352817+ephenyxshop@users.noreply.github.com>
2023-11-06 12:06:53 +0100

    8.2.3

---
+++ src/classes/shortcodes/vc_pdf.php
@@ -0,0 +1,25 @@
+<?php
+
+$vc = ephenyx_manager();
+
+extract( Composer::shortcode_atts( array(
+	'id' => '',
+	'attach_pdf' => '',
+	'btnDownloadPdf' => false,
+	'btnPrint'     => false,
+	'btnShare'     => false
+), $atts ) );
+$context = Context::getContext();
+$context->controller->addJS(_EPH_JS_DIR_ . 'pdfWorker/flipbook.min.js');
+$context->controller->addCSS(_EPH_CSS_DIR_ . 'flipbook.style.css');
+$path = ComposerMedia::getPathMediaById($id);
+$data = $context->smarty->createTemplate(_EPH_THEME_DIR_. 'script.tpl');
+
+$data->assign([
+	'pdf_dir'           => $path, 
+	'btnDownloadPdf' => $btnDownloadPdf,
+	'btnPrint' => $btnPrint,
+	'btnShare'             => $btnShare,
+]);
+
+echo $data->fetch();
