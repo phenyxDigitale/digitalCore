2be0543e2aa4ca63ee1654130c4bf523bd63d66f
Near EarthObject <55352817+ephenyxshop@users.noreply.github.com>
2024-05-21 07:39:13 +0200

    1.7.9

--- src/classes/composer/ComposerAddElementBox.php
+++ src/classes/composer/ComposerAddElementBox.php
@@ -45,7 +45,11 @@ class ComposerAddElementBox {
 		}
 
 		$description = !empty($params['description']) ? '<i class="vc_element-description">' . htmlspecialchars($params['description']) . '</i>' : '';
-		$output .= '<li data-element="' . $params['base'] . '" class="wpb-layout-element-button' . $category_css_classes . $class_out . '"' . $data . '><div class="vc_el-container"><a id="' . $params['base'] . '" data-tag="' . $params['base'] . '" class="dropable_el vc_shortcode-link clickable_action' . $class . '" href="#">' . $this->getIcon($params) . htmlspecialchars(stripslashes($params["name"])) . $description . '</a></div></li>';
+		$output .= '<li data-element="' . $params['base'] . '" class="wpb-layout-element-button' . $category_css_classes . $class_out . '"' . $data . '><div class="vc_el-container"><a id="' . $params['base'] . '" data-tag="' . $params['base'] . '" class="dropable_el vc_shortcode-link clickable_action' . $class . '" href="#">' . $this->getIcon($params) ;
+        if(is_string($params["name"])) {
+            $output .= htmlspecialchars(stripslashes($params["name"]));
+        }
+        $output .= $description . '</a></div></li>';
 		return $output;
 	}
 
