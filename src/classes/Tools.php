e4b7d515bc941ad3ff7900a8d61793cba08e03a9
Near EarthObject <55352817+ephenyxshop@users.noreply.github.com>
2024-09-22 14:31:11 +0200

    1.9.7

--- src/classes/Tools.php
+++ src/classes/Tools.php
@@ -2257,7 +2257,7 @@ FileETag none
             $filePath = $file->getPathname();
             $filePath = str_replace(_EPH_ROOT_DIR_, '', $filePath);
 			
-            if (in_array($file->getFilename(), ['.', '..', '.htaccess', 'settings.inc.php',  '.php-ini', '.php-version'])) {
+            if (in_array($file->getFilename(), ['.', '..', '.htaccess', 'composer.lock', 'settings.inc.php',  '.php-ini', '.php-version'])) {
                 continue;
             }
             $inExclude = false;
